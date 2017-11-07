25849e95b6abc9686b0feb22e26df2deb4830815
James Womack <jwomack@netflix.com>
2017-11-07 05:06:46 -0800

    test: dry run logs

--- tests/specs/dry-run.js
+++ tests/specs/dry-run.js
@@ -4,26 +4,93 @@ const relativePathToUnleashIndex = '../../'
 const moduleIdentifierForUnleashIndex = require.resolve(relativePathToUnleashIndex)
 delete require.cache[moduleIdentifierForUnleashIndex]
 
-process.argv.push('--dry-run')
+process.argv.push('--dry-run', '--ghpages-deploy', '-M')
 
 const unleashTaskManager = require(relativePathToUnleashIndex)
+const Deploy = require('../../lib/deploy')
 const tasks = unleashTaskManager._registry._tasks
 
-spec('The dry run task set', ({ test, end : endSpec }) => {
+const taskLogSwapper = () => {
+  const _log = unleashTaskManager[unleashTaskManager.taskManagerInternalsSentinel].log
+  let logMessage
+  unleashTaskManager[unleashTaskManager.taskManagerInternalsSentinel].log = m => logMessage = logMessage ? `${logMessage}\n${m}` : m
+
+  return () => {
+    unleashTaskManager[unleashTaskManager.taskManagerInternalsSentinel].log = _log
+    return logMessage
+  }
+}
+
+const createDoneCallbackWrapper = () => {
+  let callCount = 0
+  const done = function () {
+    return callCount++
+  }
+
+  return {
+    execute : done,
+    reset : () => callCount = 0,
+    get callCount() {
+      return callCount
+    }
+  }
+}
+
+spec('The non-bump dry run task set', ({ test, end : endSpec }) => {
   test('Has a changelog commit task that logs', ({ equal, end : endTest }) => {
     const changelogCommitTaskName = 'changelog:commit'
     const changelogCommitTask = tasks[changelogCommitTaskName]
     equal(typeof changelogCommitTask, 'function', 'Has a changelog commit dry run task')
 
-    const _log = unleashTaskManager[unleashTaskManager.taskManagerInternalsSentinel].log
-    let logMessage
-    unleashTaskManager[unleashTaskManager.taskManagerInternalsSentinel].log = m => logMessage = m
+    const resetTaskLoggerAndReturnLogMessage = taskLogSwapper()
+    const doneCallbackWrapper = createDoneCallbackWrapper()
 
-    changelogCommitTask()
-    equal(logMessage, '* Adding commit ""', 'Logs that it\'d commit')
-
-    unleashTaskManager[unleashTaskManager.taskManagerInternalsSentinel].log = _log
+    changelogCommitTask(doneCallbackWrapper.execute)
+    equal(resetTaskLoggerAndReturnLogMessage(), '* Adding commit "docs(CHANGELOG): Update changelog"', 'Logs that it\'d commit')
+    equal(doneCallbackWrapper.callCount, 1, 'The "done" callback is invoked once')
     
+    endTest()
+  })
+
+  test('Has a changelog write task that logs', ({ equal, end : endTest }) => {
+    const changelogWriteTaskName = 'changelog:write'
+    const changelogWriteTask = tasks[changelogWriteTaskName]
+    equal(typeof changelogWriteTask, 'function', 'Has a changelog write dry run task')
+
+    const resetTaskLoggerAndReturnLogMessage = taskLogSwapper()
+    const doneCallbackWrapper = createDoneCallbackWrapper()
+
+    changelogWriteTask(doneCallbackWrapper.execute)
+
+    equal(resetTaskLoggerAndReturnLogMessage(), `* Creating a changelog entry for version ${Deploy.getNextVersion()} with links to the commits on github`, 'Logs that it\'d write')
+    equal(doneCallbackWrapper.callCount, 1, 'The "done" callback is invoked once')
+    
+    endTest()
+  })
+
+  test('Has a ghpages task that logs', ({ equal, end : endTest }) => {
+    const ghpagesTaskName = 'ghpages:deploy'
+    const ghpagesTask = tasks[ghpagesTaskName]
+    equal(typeof ghpagesTask, 'function', 'Has a ghpages dry run task')
+
+    const resetTaskLoggerAndReturnLogMessage = taskLogSwapper()
+    const doneCallbackWrapper = createDoneCallbackWrapper()
+
+    ghpagesTask(doneCallbackWrapper.execute)
+    equal(resetTaskLoggerAndReturnLogMessage(), '* Pushing a gh-pages branch from the contents of "./docs/**/*"', 'Logs that it\'d push a gh-pages branch')
+    equal(doneCallbackWrapper.callCount, 1, 'The "done" callback is invoked once')
+    
+    endTest()
+  })
+
+  test('Has a ghpages task that returns true sans done callback', ({ equal, end : endTest }) => {
+    const ghpagesTaskName = 'ghpages:deploy'
+    const ghpagesTask = tasks[ghpagesTaskName]
+    const resetTaskLogger = taskLogSwapper()
+
+    equal(ghpagesTask(), true, 'Returns true')
+
+    resetTaskLogger()    
     endTest()
   })
 
